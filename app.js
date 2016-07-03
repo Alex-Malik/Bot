9a2d142df4ae124f301277da5d3478c23d97c53d
Alex Malik <p047a370@ya.ru>
2016-07-03 18:17:46 +0300

    Experimenting with auth cookies

--- app.js
+++ app.js
@@ -21,27 +21,82 @@ const SERVER = HTTP.createServer(onRequest).listen(process.env.PORT || 5000);
 function onRequest(request, response) {
   try {
     // Parse URL from request to handle routing
-    var url = URL.parse(request.url);
-
-    // TODO: check authorization before answering and
-    // redirect to ligin page if user is not authorized
+    var url = URL.parse(request.url, true);
 
     // Log server activity
     console.log(request.method.toLowerCase() + ': ' + url.href);
 
-    // Handle request and switch to corresponding controller method
-    if (url.pathname === '/') {
-      ROUTER.home.handle(request, response);
-    } else if (url.pathname.startsWith('/auth')) {
-      ROUTER.auth.handle(request, response, SWIG);
-    } else if (url.pathname.startsWith('/api')) {
-      ROUTER.api.handle(request, response);
-    } else if(url.pathname.startsWith('/content')) {
-      ROUTER.content.handle(request, response);
-    } else {
-      ROUTER.error.handleNotFound(request, response);
+    // // Handle request and switch to corresponding controller method
+    // if (url.pathname === '/') {
+    //   ROUTER.home.handle(request, response);
+    // } else if(url.pathname.startsWith('/content')) {
+    //   ROUTER.content.handle(request, response);
+    // } else if(auth(request, response)) {
+    //   if (url.pathname.startsWith('/api')) {
+    //     ROUTER.api.handle(request, response);
+    //   } else {
+    //     ROUTER.error.handleNotFound(request, response);
+    //   }
+    // }
+
+
+
+    // Check authorization before answering and
+    // redirect to auth page if user is not authorized
+    if (auth(request, response)) {
+      // Handle request and switch to corresponding controller method
+      if (url.pathname === '/') {
+        ROUTER.home.handle(request, response);
+      } else if(url.pathname.startsWith('/content')) {
+        ROUTER.content.handle(request, response);
+      } else if (url.pathname.startsWith('/auth')) {
+        ROUTER.auth.handle(request, response, SWIG);
+      } else if (url.pathname.startsWith('/api')) {
+        ROUTER.api.handle(request, response);
+      } else {
+        ROUTER.error.handleNotFound(request, response);
+      }
     }
   } catch(exception) {
     ROUTER.error.handleError(request, response, exception);
   }
+}
+
+function getCookies(request) {
+  if (!request) return;
+  if (!request.headers) return;
+
+  console.log(request.headers.cookie);
+
+  let cookies = {};
+
+  for (cookie of request.headers.cookie.split(';')) {
+    let pair = cookie.split('=');
+    cookies[pair[0]] = pair[1];
+  }
+
+  return cookies;
+}
+
+function setCookies(response, cookies) {
+  if (!response) return;
+  if (!response.headers) return;
+
+  console.log(response.headers.cookie);
+
+  for (cookie in cookies) {
+    response.headers.cookie[cookie] = cookie;
+  }
+}
+
+function auth(request, response) {
+  // // Parse Cookies from request to handle auth
+  // var cookies = getCookies(request);
+  // // If 'auth' cookie existing, then get user from it
+  // // and continue answering; otherwise - redirect to auth page
+  // if (!cookies.auth) {
+  //   ROUTER.auth.handle(request, response, SWIG);
+  // }
+
+  return true;
 }
