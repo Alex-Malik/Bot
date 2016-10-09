9c58057fb7417b5fd9b2fd885ba9abd557ed8736
Alex Malik <p047a370@ya.ru>
2016-10-09 18:21:27 +0300

    Add Jade/Pug; change port settings

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+// Require all used modules
 var express = require('express');
+
+// Initialize application
 var app = express();
 
+// Initialize view engine
+app.set('view engine', 'pug');
+
+// Setup public directories
+app.use('/public', express.static(__dirname + '\\public'));
+
+
+// Handle get calls
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-app.listen(5000, function () {
+// Run server
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 5000!');
 });
