aeeb91b1015f1953352fe5422bf67f4772329d46
Alex Malik <p047a370@ya.ru>
2016-10-09 16:21:14 +0300

    Clean up project; install express

--- routes/home.js
+++
@@ -1,7 +0,0 @@
-exports.handle = function (request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  //response.writeHead(302, { 'Location': '/login' });
-  //var html = jade.compileFile('.\\server\\templates\\home.jade')();
-  //response.write(html);
-  response.end();
-}
